6e408225b61e42d8681b869f4d12c87a0a2c1006
martin.selsoyvoll@hotmail.com <M6j8dhLW2vDI>
2021-05-06 12:44:04 +0200

    Setup

--- main.c
+++ main.c
@@ -2,11 +2,19 @@
  * Prosjekt_uSys_Martin.c
  *
  * Created: 06.05.2021 10:21:03
- * Author : RedBull
+ * Author : Martin Selsoyvold
  */ 
 
 #include <avr/io.h>
 
+void setup(){
+	//Setup timer
+	//Setup ADC
+	//Setup SPI/I2C
+	//Setup LCD
+	
+	//USART
+}
 
 int main(void)
 {
@@ -16,3 +24,19 @@ int main(void)
     }
 }
 
+void lcd_setup(){
+	
+}
+
+void write_to_lcd(){
+	
+}
+
+
+int read_ADC(){
+	return 1;
+}
+
+int read_ultrasonic_i2c(){
+	return 1;
+}
