62457c42cf7621e64df87e21449d1932cb64dd1f
martin.selsoyvoll@hotmail.com <M6j8dhLW2vDI>
2021-05-07 11:07:23 +0200

    I2C Fin

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
  * Created: 06.05.2021 10:21:03
  * Author : Martin Selsoyvold
  */ 
+
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/power.h>
+
 
 //LCD
 #define LCD_PORTD PORTD 
@@ -14,8 +17,9 @@
 #define RX PD0
 #define TX PD1
 //I2C ultra sonic
-#define trig PB4
-#define echo PB5
+//#define trig PB4  //SCL
+//#define echo PB5 //SDL
+#define HC_SR04_Address 0x57
 //Other
 #define Button PB1
 #define R_LED PB2
@@ -36,42 +40,32 @@ void setup(){
 
 int main(void)
 {
+	clock_prescale_set(clock_div_1); 
 	lcd_setup();
-    /* Replace with your application code */
+	I2C_Setup();
+	
 	write_to_lcd("test");	//Begin writing at Line 1, Position 1
+	
+	
     while (1) 
     {
-		char showruntime [16];
-		itoa (timeout_timer,showruntime,10);
+		I2C_Start();
+		I2C_Send(HC_SR04_Address);
+		int HighByte = I2C_Read_High();
+		int LowByte = I2C_Read_Low();
+		lcd_func(0x80);		//Go to Line 1 Pos 1
+		write_to_lcd(LowByte);
 		lcd_func(0xC0);		//Go to Line 2, Position 1
-		write_to_lcd("RUNTIME (s): ");
-		write_to_lcd(showruntime);
+		write_to_lcd(HighByte);
+		I2C_Stop();
+
 		_delay_ms(1000);
 		timeout_timer++;
     }
 }
 
+
 /*
-void I2C_Init(void){
-	---TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN); // Step 1: Start TWI/I2C (Clear TWINT flag)
-	---while (!(TWCR & (1<<TWINT))){} //Step 2: Wait for TWINT to be set (Possible use of interupt later)
-	if ((TWSR & 0xF8) != START){} //Step 3: Check if TWSR is equal to the start condition
-
-	TWDR = SLA_W; // Step 3.1: Assuming that the status code is as expected, the application must load SLA+W into TWDR.
-	TWCR = (1<<TWINT) | (1<<TWEN); //Step 4: When the address packet has been transmitted, the TWINT Flag in TWCR is set, and TWSR is updated with a status code indicating that the address packet has successfully been sent.
-	while(!(TWCR & (1<<TWINT))){} //Step 5: The application software should now examine the value of TWSR, to make sure that the address packet was successfully transmitted, and that the value of the ACK bit was as expected.
-	
-	if ((TWSR & 0xF8) != SLA_ACK){} //Step 6: When the data packet has been transmitted, the TWINT Flag in TWCR is set, and TWSR is updated with a status code indicating that the data packet has successfully been sent.
-	
-	---TWDR = DATA;
-	---TWCR = (1<<TWINT) | (1<<TWEN);
-	while (!(TWCR & (1<<TWINT))){}
-	if ((TWSR & 0xF8) != DATA_ACK){} // Step 7: The application software should now examine the value of TWSR, to make sure that the data packet was successfully transmitted
-	---TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO); //Step 8: Stop I2C
-}
-
-*/
-
 void I2C_Setup(){
 	TWBR = 32; 
 	TWCR |= (1 << TWEN);
@@ -79,40 +73,44 @@ void I2C_Setup(){
 
 void I2C_Start(){
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);  // Step 1: Start TWI/I2C (Clear TWINT flag)
-	while (!(TWCR & (1<<TWINT))){} //Step 2: Wait for TWINT to be set
+	//while (!(TWCR & (1<<TWINT))){} //Step 2: Wait for TWINT to be set
+	loop_until_bit_is_set(TWCR, TWINT);
 }
 
 int I2C_Read_High(){
 	TWCR = (1<<(TWINT) | (1<<TWEN) | (1<<TWEA));//TWI Enable Acknowledge Bit
-	while (!(TWCR & (1<<TWINT))){}
+	//while (!(TWCR & (1<<TWINT))){}
+	loop_until_bit_is_set(TWCR, TWINT);
 	return (TWDR);
 }
 
 
 int I2C_Read_Low(){
 		TWCR = (1<<(TWINT) | (1<<TWEN));//TWI Enable Acknowledge Bit
-		while (!(TWCR & (1<<TWINT))){}
+		//while (!(TWCR & (1<<TWINT))){}
+		loop_until_bit_is_set(TWCR, TWINT);
 		return (TWDR);
 }
 
 void I2C_Send(int DATA){
 	TWDR = DATA;
 	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (!(TWCR & (1<<TWINT))){}
+	//while (!(TWCR & (1<<TWINT))){}
+	loop_until_bit_is_set(TWCR, TWINT);
 }
 
 void I2C_Stop(){
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO); //Step 8: Stop I2C
 }
-
+*/
 
 void lcd_setup(){
 	LCD_PIND = 0xFF;
 	_delay_ms(15);
 	lcd_func(0x02);		//4-Bit Control
 	lcd_func(0x28);     //Control Matrix @ 4-Bit
-	//lcd_func(0x0c);     //Disable Cursor
-	//lcd_func(0x06);     //Move Cursor
+	lcd_func(0x0c);     //Disable Cursor
+	lcd_func(0x06);     //Move Cursor
 	clear_lcd();
 	_delay_ms(2);
 }
